72fbc5002db57e696ed806e9181cb63948f774d2
Jterrettaz <jterrettaz@gmail.com>
2022-06-05 08:54:04 +0200

    Archive records without loop data
    Added calculation of sunshine duration for archive record extracted from datalogger at start  or first archive record after weewx start since loop data are not available for these archive records

--- sunduration.py
+++ sunduration.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import time
 import weewx
 from weewx.wxengine import StdService
-import schemas.wview
 
 try:
     # Test for new-style weewx logging by trying to import weeutil.logger
@@ -46,8 +45,6 @@ except ImportError:
         logmsg(syslog.LOG_ERR, msg)
 
 
-
-
 class SunshineDuration(StdService):
     def __init__(self, engine, config_dict):
         # Pass the initialization information on to my superclass:
@@ -59,51 +56,70 @@ class SunshineDuration(StdService):
         self.lastdateTime = 0
         self.LoopDuration = 0
         self.sunshineSeconds = 0
-
-
+        self.lastSeuil = 0
+        self.firstArchive = True
 
     def newLoopPacket(self, event):
-        """Gets called on a new archive record event."""
-        seuil = 0
-        coeff = 0.9
+        """Gets called on a new loop packet event."""
         radiation = event.packet.get('radiation')
         if radiation is not None:
             if self.lastdateTime == 0:
                 self.lastdateTime = event.packet.get('dateTime')
-            self.LoopDuration=event.packet.get('dateTime')-self.lastdateTime
+            self.LoopDuration = event.packet.get('dateTime') - self.lastdateTime
             self.lastdateTime = event.packet.get('dateTime')
-            utcdate = datetime.utcfromtimestamp(event.packet.get('dateTime'))
-            
-            dayofyear = int(time.strftime("%j", time.gmtime(event.packet.get('dateTime'))))
-            theta = 360 * dayofyear / 365
-            equatemps = 0.0172 + 0.4281 * cos((pi / 180) * theta) - 7.3515 * sin(
-                (pi / 180) * theta) - 3.3495 * cos(2 * (pi / 180) * theta) - 9.3619 * sin(
-                2 * (pi / 180) * theta)
-
-            latitude = float(self.config_dict["Station"]["latitude"])
-            longitude = float(self.config_dict["Station"]["longitude"])
-            corrtemps = longitude * 4
-            declinaison = asin(0.006918 - 0.399912 * cos((pi / 180) * theta) + 0.070257 * sin(
-                (pi / 180) * theta) - 0.006758 * cos(2 * (pi / 180) * theta) + 0.000908 * sin(
-                2 * (pi / 180) * theta)) * (180 / pi)
-
-            minutesjour = utcdate.hour * 60 + utcdate.minute
-            tempsolaire = (minutesjour + corrtemps + equatemps) / 60
-            angle_horaire = (tempsolaire - 12) * 15
-            hauteur_soleil = asin(sin((pi / 180) * latitude) * sin((pi / 180) * declinaison) + cos(
-                (pi / 180) * latitude) * cos((pi / 180) * declinaison) * cos((pi / 180) * angle_horaire)) * (180 / pi)
-            if hauteur_soleil > 3:
-                seuil = (0.73 + 0.06 * cos((pi / 180) * 360 * dayofyear / 365)) * 1080 * pow(
-                    (sin(pi / 180) * hauteur_soleil), 1.25) * coeff
-                if radiation > seuil:
-                    self.sunshineSeconds += self.LoopDuration
-
-            logdbg("Calculated LOOP sunshine duration (seconds) = %f, based on radiation = %f, and threshold = %f" % (self.LoopDuration, radiation, seuil))
-
+            seuil = self.sunshineThreshold(event.packet.get('dateTime'))
+            if radiation > seuil and radiation > 20:
+                self.sunshineSeconds += self.LoopDuration
+            self.lastSeuil = seuil
+            logdbg("Calculated LOOP sunshine_time = %f, based on radiation = %f, and threshold = %f" % (
+                self.LoopDuration, radiation, seuil))
 
     def newArchiveRecord(self, event):
-        event.record['sunshine_time'] = self.sunshineSeconds/60
+        """Gets called on a new archive record event."""
+        if self.lastdateTime == 0 or self.firstArchive:  # LOOP packets not yet captured. This is an archive record extracted from datalogger at start  or first archive record after weewx start
+            radiation = event.record.get('radiation')
+            event.record['sunshine_time'] = 0.0
+            event.record['sunshineThreshold'] = 0.0
+            if radiation is not None:
+                seuil = self.sunshineThreshold(event.record.get('dateTime'))
+                self.lastSeuil = seuil
+                if radiation > seuil and radiation > 20:
+                    event.record['sunshine_time'] = event.record['interval']
+                if self.lastdateTime != 0:  # LOOP already started, this is the first regular archive after weewx start
+                    self.firstArchive = False
+                event.record['sunshineThreshold'] = self.lastSeuil
+            loginf("Estimated sunshine duration from archive record= %f min, radiation = %f, and threshold = %f" % (
+                event.record['sunshine_time'], event.record['radiation'], event.record['sunshineThreshold']))
+        else:
+            event.record['sunshineThreshold'] = self.lastSeuil
+            event.record['sunshine_time'] = self.sunshineSeconds / 60
+            loginf("Sunshine duration from loop packets = %f min, last radiation = %f, and last threshold = %f" % (
+                event.record['sunshine_time'], event.record['radiation'], event.record['sunshineThreshold']))
+
         self.sunshineSeconds = 0
-        loginf("Calculated sunshine duration (minutes) for archive period = %f" % (event.record['sunshine_time']))
-        
-    schema_with_sunshine_time = schemas.wview.schema + [('sunshine_time', 'REAL')]
+
+    def sunshineThreshold(self, mydatetime):
+        coeff = 0.9  # change to calibrate with your sensor
+        utcdate = datetime.utcfromtimestamp(mydatetime)
+        dayofyear = int(time.strftime("%j", time.gmtime(mydatetime)))
+        theta = 360 * dayofyear / 365
+        equatemps = 0.0172 + 0.4281 * cos((pi / 180) * theta) - 7.3515 * sin(
+            (pi / 180) * theta) - 3.3495 * cos(2 * (pi / 180) * theta) - 9.3619 * sin(
+            2 * (pi / 180) * theta)
+
+        latitude = float(self.config_dict["Station"]["latitude"])
+        longitude = float(self.config_dict["Station"]["longitude"])
+        corrtemps = longitude * 4
+        declinaison = asin(0.006918 - 0.399912 * cos((pi / 180) * theta) + 0.070257 * sin(
+            (pi / 180) * theta) - 0.006758 * cos(2 * (pi / 180) * theta) + 0.000908 * sin(
+            2 * (pi / 180) * theta)) * (180 / pi)
+
+        minutesjour = utcdate.hour * 60 + utcdate.minute
+        tempsolaire = (minutesjour + corrtemps + equatemps) / 60
+        angle_horaire = (tempsolaire - 12) * 15
+        hauteur_soleil = asin(sin((pi / 180) * latitude) * sin((pi / 180) * declinaison) + cos(
+            (pi / 180) * latitude) * cos((pi / 180) * declinaison) * cos((pi / 180) * angle_horaire)) * (180 / pi)
+        seuil = (0.73 + 0.06 * cos((pi / 180) * 360 * dayofyear / 365)) * 1080 * pow(
+            (sin(pi / 180) * hauteur_soleil), 1.25) * coeff
+        return seuil
+
